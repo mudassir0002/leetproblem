188b21c3424f4c405c05ecfa172811d4d6405c51
mudassir0002 <mudassirsiddiqui03@gmail.com>
2024-09-27 06:24:24 +0530

    Add Check if Array Is Sorted and Rotated

--- Check_if_Array_Is_Sorted_and_Rotated.cpp
+++ Check_if_Array_Is_Sorted_and_Rotated.cpp
@@ -2,11 +2,12 @@ class Solution {
 public:
     bool check(vector<int>& nums) {
         int small=0;
-        for(int i=1 ; i<nums.size() ;i++){
+        for(int i=0 ; i<nums.size() ;i++){
             if(nums[small]>nums[i]){
                 small=i;
             }
         }
+        int flag=0;
         for(int i = 0 ;i<nums.size()-1;i++){
             int index = (small + i)%nums.size();
             if(nums[index]>nums[(index+1)%nums.size()]){
