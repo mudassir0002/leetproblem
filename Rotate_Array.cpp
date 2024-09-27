91db0e1beeca5f27bde19e9ba52ad9ceb1a6bcd0
mudassir0002 <mudassirsiddiqui03@gmail.com>
2024-09-27 14:31:46 +0530

    Add Rotate Array

--- Rotate_Array.cpp
+++ Rotate_Array.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     void rotate(vector<int>& arr, int k) {
         int n = arr.size();
-        k = k % n;
         for(int i=0 ; i<k ;i++){
             int temp=arr[n-1];
             for(int j=n-1 ;j>0 ;j--){
