810ff96c930c772340d2cbb7760be5cf2b793f2b
mudassir0002 <mudassirsiddiqui03@gmail.com>
2024-09-27 15:40:13 +0530

    Add Rotate Array

--- Rotate_Array.cpp
+++ Rotate_Array.cpp
@@ -4,6 +4,6 @@ public:
         int n = arr.size();
         reverse(arr.begin(),arr.begin() + k);
         reverse(arr.begin() + k,arr.begin() + arr.end());
-        reverse(arr.begin() ,arr.begin() + arr.end());
+        reverse(arr.begin() ,arr.begin() + arr.end())
     }
 };
