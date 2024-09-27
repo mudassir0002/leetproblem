e1ce16d93425d2392b8bc39db307778cc8c21bc1
mudassir0002 <mudassirsiddiqui03@gmail.com>
2024-09-27 15:42:50 +0530

    Add Rotate Array

--- Rotate_Array.cpp
+++ Rotate_Array.cpp
@@ -1,18 +1,10 @@
-#include <vector>
-#include <algorithm> // for std::reverse
-
 class Solution {
 public:
     void rotate(vector<int>& arr, int k) {
         int n = arr.size();
-        k = k % n; // to handle cases where k > n
-        if (k == 0) return; // no rotation needed if k is 0
-        
-     
-        reverse(arr.begin(), arr.end());
-        // Step 2: Reverse the first k elements
-        reverse(arr.begin(), arr.begin() + k);
-        // Step 3: Reverse the remaining n - k elements
+        reverse(arr.begin() ,arr.end());
+        reverse(arr.begin(),arr.begin() + k);
         reverse(arr.begin() + k, arr.end());
+        
     }
 };
