61df92618b312a3b7626cc04372ef94eb04c32fd
mudassir0002 <mudassirsiddiqui03@gmail.com>
2024-12-28 05:50:46 +0530

    Add Reverse Integer

--- Reverse_Integer.java
+++ Reverse_Integer.java
@@ -3,6 +3,11 @@ class Solution {
         int temp=x;
         int rev=0;
         while(temp!=0){
+
+            if(rev > Integer.MAX_VALUE/10 || rev < Integer.MIN_VALUE/10){
+                return 0;
+            }
+
             rev = rev*10+temp%10; 
             temp /=10;
         }
