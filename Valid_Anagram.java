df51f62820160adfcdc4dc85b303e4278e779d85
mudassir0002 <mudassirsiddiqui03@gmail.com>
2025-01-06 04:54:35 +0530

    Add Valid Anagram

---
+++ Valid_Anagram.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        // // If the lengths are different, they cannot be anagrams
+        if (s.length() != t.length()) {
+            return false;
+        }
+        
+        // // Convert strings to character arrays
+        // char[] sArray = s.toCharArray();
+        // char[] tArray = t.toCharArray();
+
+        // // Sort the character arrays
+        // Arrays.sort(sArray);
+        // Arrays.sort(tArray);
+
+        // // Compare the sorted character arrays
+        // return Arrays.equals(sArray, tArray);
+        int m1[] = new int[26];
+        
+        for(int i=0 ;i<s.length() ; ++i){
+            m1[s.charAt(i) -'a']++;
+        }
+        for(int i=0 ;i<t.length() ; ++i){
+            m1[t.charAt(i)-'a']--;
+        }
+        for(int i=0 ; i<m1.length ; i++){
+            if(m1[i]!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
