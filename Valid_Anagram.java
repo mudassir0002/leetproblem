e38c33ffb52ee6e724dbbc7fa2d3f44768c14402
mudassir0002 <mudassirsiddiqui03@gmail.com>
2025-01-06 04:34:32 +0530

    Add Valid Anagram

--- Valid_Anagram.java
+++ Valid_Anagram.java
@@ -2,32 +2,20 @@ import java.util.Arrays;
 
 class Solution {
     public boolean isAnagram(String s, String t) {
-        // // If the lengths are different, they cannot be anagrams
+        // If the lengths are different, they cannot be anagrams
         if (s.length() != t.length()) {
             return false;
         }
         
-        // // Convert strings to character arrays
-        // char[] sArray = s.toCharArray();
-        // char[] tArray = t.toCharArray();
+        // Convert strings to character arrays
+        char[] sArray = s.toCharArray();
+        char[] tArray = t.toCharArray();
 
-        // // Sort the character arrays
-        // Arrays.sort(sArray);
-        // Arrays.sort(tArray);
+        // Sort the character arrays
+        Arrays.sort(sArray);
+        Arrays.sort(tArray);
 
-        // // Compare the sorted character arrays
-        // return Arrays.equals(sArray, tArray);
-        char m1[] = new char[27];
-        
-        for(int i=0 ;i<s.length() ; i++){
-            m1[s.charAt(i)-'a']++;
-            m1[t.charAt(i)-'a']--;
-        }
-        for(int i=0 ; i<s.length() ; i++){
-            if(m1[i]!=0){
-                return false;
-            }
-        }
-        return true;
+        // Compare the sorted character arrays
+        return Arrays.equals(sArray, tArray);
     }
 }
