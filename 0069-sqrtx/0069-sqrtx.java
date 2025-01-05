293c76cde07ed6b2c08d9d96567acf24ed348bdb
Mudassir Ahmed <134137854+mudassir0002@users.noreply.github.com>
2025-01-05 11:44:15 +0530

    Time: 1 ms (87.09%), Space: 41.1 MB (35.37%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,5 +1,17 @@
 class Solution {
     public int mySqrt(int x) {
-        return (int)Math.sqrt(x);
+        // return (int)Math.sqrt(x);
+        int l=0;
+        int h=x;
+        while(l<=h){
+            long mid = l + (h-l)/2;
+            long val = mid*mid;
+            if(val <=(long)x){
+                l=(int)mid+1;
+            }else{
+                h = (int)mid-1;
+            }
+        }
+        return h;
     }
 }
