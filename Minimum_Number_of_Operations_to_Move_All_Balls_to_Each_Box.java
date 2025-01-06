00614914eb776bb51c3f0e06f554b221db9a2fc4
mudassir0002 <mudassirsiddiqui03@gmail.com>
2025-01-06 14:49:09 +0530

    Add Minimum Number of Operations to Move All Balls to Each Box

---
+++ Minimum_Number_of_Operations_to_Move_All_Balls_to_Each_Box.java
@@ -0,0 +1,18 @@
+class Solution {
+    public static int[] minOperations(String boxes) {
+        int n = boxes.length();
+        int[] answer = new int[n];
+
+        for (int i = 0; i < n; ++i) {
+            int count = 0;
+            for (int j = 0; j < n; ++j) {
+                if (boxes.charAt(j) == '1') {
+                    count += Math.abs(i - j);
+                }
+            }
+            answer[i] = count;
+        }
+
+        return answer;
+    }
+}
