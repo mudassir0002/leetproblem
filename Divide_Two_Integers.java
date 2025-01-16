8c92348eba37243db08766f04e42629ac6737046
mudassir0002 <mudassirsiddiqui03@gmail.com>
2025-01-16 11:05:42 +0530

    Add Divide Two Integers

--- Divide_Two_Integers.java
+++ Divide_Two_Integers.java
@@ -1,11 +1,5 @@
 class Solution {
     public int divide(int dividend, int divisor) {
-        if(divisor!=0){
-            if(dividend<0 && divisor<0){
-                return -1*dividend -1;
-            }
-            return (dividend/divisor);
-        }
-        return 0;
+        return dividend/divisor;
     }
 }
