69481e37b86db3bbce4482a9b3af60affd6d08c7
mudassir0002 <mudassirsiddiqui03@gmail.com>
2025-01-16 09:02:37 +0530

    Add Power of Two

--- Power_of_Two.java
+++ Power_of_Two.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        if((n & (n-1))==0 && n>0){
+        if((n & (n-1))==0){
             return true;
         }else{
             return false;
