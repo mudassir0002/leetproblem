3d9661f10989c0aa215cdb91dd8473f0ac60f80d
mudassir0002 <mudassirsiddiqui03@gmail.com>
2025-02-27 15:06:21 +0530

    Add Length of Longest Fibonacci Subsequence

--- Length_of_Longest_Fibonacci_Subsequence.java
+++ Length_of_Longest_Fibonacci_Subsequence.java
@@ -5,7 +5,7 @@ class Solution {
             set.add(num);
         }
 
-        int maxLength = 0;
+        int maxLength = 0
         int n = arr.length;
 
         for (int i = 0; i < n - 1; i++) {
