c6b12508693b4ace7347653fd601bffdf5693b42
mudassir0002 <mudassirsiddiqui03@gmail.com>
2025-03-01 08:12:04 +0530

    Add Apply Operations to an Array

---
+++ Apply_Operations_to_an_Array.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] applyOperations(int[] nums) {
+        int n = nums.length;
+
+            for(int j=0 ; j<n-1 ; j++){
+                if(nums[j]==nums[j+1]){
+                    nums[j] = nums[j] *2;
+                    nums[j+1]=0;
+                }
+            }
+        int count=0;
+        int k=0;
+        for(int i=0 ; i<n ; i++){
+            if(nums[i]!=0){
+                nums[k++] = nums[i];
+            }else{
+                count++;
+            }
+        }
+
+        for(int i=k ; i<k+count ; i++){
+            nums[i]=0;
+        }
+        
+        return nums;
+    }
+}
