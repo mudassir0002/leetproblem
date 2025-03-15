b712714e4e265f5f07b2b5093ecc52da1ac9f5d4
mudassir0002 <mudassirsiddiqui03@gmail.com>
2025-03-15 14:48:06 +0530

    Add House Robber IV

---
+++ House_Robber_IV.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int minCapability(int[] nums, int k) {
+        int n=nums.length;
+        int max=Integer.MIN_VALUE,min=Integer.MAX_VALUE;
+        for(int i=0;i<n;i++)
+        {
+            min=Math.min(min,nums[i]);
+            max=Math.max(max,nums[i]);
+        }
+        int low=min,high=max;
+        int ans=0;
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+            if(valid(nums,mid,k))
+            {
+                ans=mid;
+                high=mid-1;
+            }
+            else
+            {
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+    public boolean valid(int nums[],int mid,int k)
+    {
+        int c=0;
+        int i=0;
+        while(i<nums.length)
+        {
+         if(nums[i]<=mid)
+         {
+            c++;
+            i+=2;
+         }
+         else
+         {
+            i++;
+         }
+         if(c==k)
+          return true;
+        }
+       
+        return false;
+    }
+}
