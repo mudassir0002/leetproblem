26c46ad407ff95b6f342d45516b5a1b74efcb8b4
mudassir0002 <mudassirsiddiqui03@gmail.com>
2025-03-17 14:48:21 +0530

    Add Divide Array Into Equal Pairs

---
+++ Divide_Array_Into_Equal_Pairs.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean divideArray(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        
+        for(int i=0 ; i<n ;i=i+2){
+            if(nums[i]!=nums[i+1]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
