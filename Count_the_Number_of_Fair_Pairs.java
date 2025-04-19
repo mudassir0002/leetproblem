3e83782af704df18fab67fdf7dd3695079ef273f
mudassir0002 <mudassirsiddiqui03@gmail.com>
2025-04-19 19:17:13 +0530

    Add Count the Number of Fair Pairs

--- Count_the_Number_of_Fair_Pairs.java
+++ Count_the_Number_of_Fair_Pairs.java
@@ -1,10 +1,34 @@
 class Solution {
     public long countFairPairs(int[] nums, int lower, int upper) {
         long ans = 0;
-        for(int i = 0; i < nums.length; i++)
-            for(int j = i+1; j < nums.length; j++)
-                if(nums[i] + nums[j] >= lower && nums[i] + nums[j] <= upper)
-                    ++ans;
+        Arrays.sort(nums);
+        for (int i = 0; i + 1 < nums.length; i++) {
+            int minReq = lower - nums[i];
+            int maxReq = upper - nums[i];
+            int low = lowerBound(nums, i + 1, nums.length, minReq);
+            int high = upperBound(nums, i + 1, nums.length, maxReq);
+            ans += (high - low);
+        }
         return ans;
+    }
+
+    private int lowerBound(int[] nums, int start, int end, int target) {
+        int lo = start, hi = end;
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (nums[mid] < target) lo = mid + 1;
+            else hi = mid;
+        }
+        return lo;
+    }
+
+    private int upperBound(int[] nums, int start, int end, int target) {
+        int lo = start, hi = end;
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (nums[mid] <= target) lo = mid + 1;
+            else hi = mid;
+        }
+        return lo;
     }
 }
