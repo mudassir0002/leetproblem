ff80d77188bfe91722ceb7ef2281b831036cdcb7
mudassir0002 <mudassirsiddiqui03@gmail.com>
2025-05-19 11:13:52 +0530

    Add Type of Triangle

--- Type_of_Triangle.java
+++ Type_of_Triangle.java
@@ -4,10 +4,8 @@ class Solution {
             return "equilateral";
         }else if((nums[0]==nums[1]) || (nums[1]==nums[2]) || (nums[0]==nums[2])){
             return "isosceles";
-        }else if ((nums[0]+nums[1]>nums[2]) && (nums[0]+nums[2]>nums[1]) && (nums[1]+nums[2]>nums[0]) ){
-            return "scalene";
         }else{
-            return "none";
+            return "scalene";
         }
     }
 }
