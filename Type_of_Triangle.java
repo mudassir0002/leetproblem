399a7489981b9cceb10fc850cdf85e7bbe45fa04
mudassir0002 <mudassirsiddiqui03@gmail.com>
2025-05-19 11:19:03 +0530

    Add Type of Triangle

--- Type_of_Triangle.java
+++ Type_of_Triangle.java
@@ -1,16 +1,13 @@
 class Solution {
     public String triangleType(int[] nums) {
-        if ((nums[0]+nums[1]>nums[2]) && (nums[0]+nums[2]>nums[1]) && (nums[1]+nums[2]>nums[0]) ){
-            if(nums[0]==nums[1] && nums[1]==nums[2] && nums[0]==nums[2]){
+        if(nums[0]==nums[1] && nums[1]==nums[2] && nums[0]==nums[2]){
             return "equilateral";
         }else if((nums[0]==nums[1]) || (nums[1]==nums[2]) || (nums[0]==nums[2])){
             return "isosceles";
-        }else{
+        }else if ((nums[0]+nums[1]>nums[2]) && (nums[0]+nums[2]>nums[1]) && (nums[1]+nums[2]>nums[0]) ){
             return "scalene";
-        }
         }else{
             return "none";
         }
-        
     }
 }
