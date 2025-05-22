037a347ee9b182123dd5360421d654411f7e292e
Mudassir Ahmed <134137854+mudassir0002@users.noreply.github.com>
2025-05-22 20:48:39 +0530

    Time: 546 ms (5.23%), Space: 45.9 MB (77.67%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -13,15 +13,45 @@ class Solution {
         //     return nums[nums.length-1];
         // }
         // }
-
+        int n = nums.length;
         if(nums.length==1 || nums.length==0){
             return nums[0];
         }
-        int ans= nums[0];
-        for(int i=1 ; i<nums.length ;i++){
-            ans = ans ^ nums[i];
-
+        int count=0;
+        for(int i=0 ; i<n ; i++){
+            int x = nums[i];
+            for(int j=0 ; j<n ;j++){
+                if(nums[j]==x){
+                    count++;
+                }
+            }
+            if(count==1){
+                return nums[i];
+            }
+            count=0;
         }
-        return ans;
+        return -1;
+
+        
+        // int arr[] = new int[n];
+        // for(int i=0 ;i<n ; i++){
+        //     arr[nums[i]]++;
+        // }
+        // for(int i=0 ;i<n ; i++){
+        //     if(arr[i]==1){
+        //         return i;
+        //     }
+        // }
+        // return -1;
+
+
+        // if(nums.length==1 || nums.length==0){
+        //     return nums[0];
+        // }
+        // int ans= nums[0];
+        // for(int i=1 ; i<nums.length ;i++){
+        //     ans = ans ^ nums[i];
+        // }
+        // return ans;
     }
 }
