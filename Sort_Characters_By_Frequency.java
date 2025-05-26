66de165c9f1318f499fe0230693a18661d363d89
mudassir0002 <mudassirsiddiqui03@gmail.com>
2025-05-26 10:00:03 +0530

    Add Sort Characters By Frequency

--- Sort_Characters_By_Frequency.java
+++ Sort_Characters_By_Frequency.java
@@ -1,9 +1,26 @@
 class Solution {
     public String frequencySort(String s) {
-        String rev = "";
+        StringBuilder sb = new StringBuilder();
+        int arr[] = new int[256];
         for (int i = 0; i < s.length(); i++) {
-            rev = s.charAt(i) + rev;
+            arr[s.charAt(i)]++;
         }
-        return rev.toString();
+        int max=0;
+        int ind=0;
+        for (int i = 0; i < 256; i++) {
+            for(int j=0 ; j<256 ; j++){
+                if(max<arr[j]){
+                    max = arr[j];
+                    ind = j;
+                }
+            }
+            for(int k=0 ; k<max ; k++){
+                sb.append((char)ind);
+            }
+            arr[ind]=0;
+            max=0;
+            ind=0;
+        }
+        return sb.toString();
     }
 }
