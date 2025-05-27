b9388da97ba8c6078fde63bbae930a3802ba9bce
mudassir0002 <mudassirsiddiqui03@gmail.com>
2025-05-27 19:01:04 +0530

    Add Divisible and Non-divisible Sums Difference

---
+++ Divisible_and_Non-divisible_Sums_Difference.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int differenceOfSums(int n, int m) {
+        int cnt1 = 0,cnt2=0;
+        for(int i=1 ; i<=n ; i++){
+            if(i%m==0){
+                cnt2 +=i;
+            }else{
+                cnt1 +=i;
+            }
+        }
+        return cnt1 - cnt2;
+    }
+}
