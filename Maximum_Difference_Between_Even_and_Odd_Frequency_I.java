f666ae9c82734db1cae3971bdc1a17a4a307409c
mudassir0002 <mudassirsiddiqui03@gmail.com>
2025-06-10 07:16:06 +0530

    Add Maximum Difference Between Even and Odd Frequency I

--- Maximum_Difference_Between_Even_and_Odd_Frequency_I.java
+++ Maximum_Difference_Between_Even_and_Odd_Frequency_I.java
@@ -1,18 +1,23 @@
 class Solution {
     public int maxDifference(String s) {
-        int[] mpp = new int[26];
-        int maxi = 0, mini = s.length();
-        for (char c : s.toCharArray()) {
-            mpp[c - 'a']++;
+        int arr[] = new int[26];
+        for(int i=0 ; i<s.length() ; i++){
+            arr[ s.charAt(i) - 'a']++;
         }
-        for (int i = 0; i < 26; i++) {
-            if (mpp[i] % 2 != 0) {
-                maxi = Math.max(maxi, mpp[i]);
-            }
-            if (mpp[i] % 2 == 0 && mpp[i] > 0) {
-                mini = Math.min(mini, mpp[i]);
+        int maxodd = 0;
+        int maxeven =0;
+        for(int i=0 ; i<arr.length ; i++){
+            if(arr[i]%2==0){
+                maxeven = Math.max(maxeven , arr[i]);
+            }else{
+                maxodd = Math.max(maxodd , arr[i]);
             }
         }
-        return maxi - mini;
+        if(maxodd-maxeven < 0){
+            return -1;
+        }else{
+            return maxodd-maxeven;
+        }
+        
     }
 }
