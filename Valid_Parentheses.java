1e20bbacd6b93be36d482a5026dde1c701abb9aa
mudassir0002 <mudassirsiddiqui03@gmail.com>
2025-06-16 10:43:25 +0530

    Add Valid Parentheses

--- Valid_Parentheses.java
+++ Valid_Parentheses.java
@@ -2,17 +2,12 @@ class Solution {
     public boolean isValid(String s) {
         Stack st = new Stack();
         for(int i=0 ;i<s.length() ; i++){
-            
             if(s.charAt(i)=='(' || s.charAt(i)=='{' || s.charAt(i)=='['){
                 st.push(s.charAt(i));
+            }else if((s.charAt(i)==')' && st.peek().equals('(')) || (s.charAt(i)==']' && st.peek().equals('[')) ||(s.charAt(i)=='}' && st.peek().equals('{'))){
+                st.pop();
             }else{
-                if (st.isEmpty()) return false;
-                if((s.charAt(i)==')' && st.peek().equals('(')) ||
-                    (s.charAt(i)==']' && st.peek().equals('[')) || 
-                    (s.charAt(i)=='}' && st.peek().equals('{')))
-                    {
-                    st.pop();
-                    }
+                return false;
             }
         }
         if(st.isEmpty()){
