847a1c2c371e8e07fffa9a536a64b0e192776449
Mudassir Ahmed <134137854+mudassir0002@users.noreply.github.com>
2025-06-18 13:11:32 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        // Arrays.sort(bills);
+        int amount[] = new int[3];
+        for(int i=0 ;i<bills.length ; i++){
+            if(bills[i]==5){
+                amount[0] +=5;
+            }else if(bills[i]==10){
+                if(amount[0]>=5){
+                    amount[0] -=5;
+                    amount[1] +=10;
+                }else{
+                    return false;
+                }
+            }else{
+                if(amount[0]>=5 && amount[1]>=10){
+                    amount[0] -=5;
+                    amount[1] -=10;
+                    amount[2] +=20;
+                }else{
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
