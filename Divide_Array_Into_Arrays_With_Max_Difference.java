27bd77f366a27b095743e45cc67779d936a70baa
mudassir0002 <mudassirsiddiqui03@gmail.com>
2025-06-18 05:50:08 +0530

    Add Divide Array Into Arrays With Max Difference

--- Divide_Array_Into_Arrays_With_Max_Difference.java
+++ Divide_Array_Into_Arrays_With_Max_Difference.java
@@ -1,24 +1,27 @@
-import java.util.*;
-
 class Solution {
     public int[][] divideArray(int[] nums, int k) {
         int n = nums.length;
-        if (n % 3 != 0) {
-            return new int[0][];
+        int a[][] = new int[n/3][3];
+        if(n%3!=0){
+            return a;
         }
-
         Arrays.sort(nums);
-        int[][] result = new int[n / 3][3];
-
-        for (int i = 0; i < n; i += 3) {
-            if (nums[i + 2] - nums[i] > k) {
+        int i=0;
+        int count=0;
+        while(i<n){
+            if( (Math.abs(nums[i]-nums[i+1]) <= k) &&
+                (Math.abs(nums[i+1]-nums[i+2]) <= k) &&
+                (Math.abs(nums[i]-nums[i+2]) <= k) 
+              ){
+                a[count][0] = nums[i];
+                a[count][1] = nums[i+1];
+                a[count][2] = nums[i+2];
+            }else{
                 return new int[0][];
             }
-            result[i / 3][0] = nums[i];
-            result[i / 3][1] = nums[i + 1];
-            result[i / 3][2] = nums[i + 2];
+            i=i+3;
+            count++;
         }
-
-        return result;
+        return a;
     }
 }
