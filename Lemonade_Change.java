38881fdece4ae9636c3cec82e098e3341a7df74a
mudassir0002 <mudassirsiddiqui03@gmail.com>
2025-06-18 07:41:55 +0530

    Add Lemonade Change

--- Lemonade_Change.java
+++ Lemonade_Change.java
@@ -1,27 +1,26 @@
 class Solution {
     public boolean lemonadeChange(int[] bills) {
-        Arrays.sort(bills);
-        int amount[] = new int[3];
-        for(int i=0 ;i<bills.length ; i++){
-            if(bills[i]==5){
-                amount[0] +=5;
-            }else if(bills[i]==10){
-                if(amount[0]>=5){
-                    amount[0] -=5;
-                    amount[1] +=10;
-                }else{
-                    return false;
-                }
-            }else{
-                if(amount[0]>=5 && amount[1]>=10){
-                    amount[0] -=5;
-                    amount[1] -=10;
-                    amount[2] +=20;
-                }else{
+        int five = 0, ten = 0;
+        
+        for (int bill : bills) {
+            if (bill == 5) {
+                five++;
+            } else if (bill == 10) {
+                if (five == 0) return false;
+                five--;
+                ten++;
+            } else { // bill == 20
+                if (ten > 0 && five > 0) {
+                    ten--;
+                    five--;
+                } else if (five >= 3) {
+                    five -= 3;
+                } else {
                     return false;
                 }
             }
         }
+        
         return true;
     }
 }
