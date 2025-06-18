758425b55130ec02f32fd067c99a8056d024ab63
mudassir0002 <mudassirsiddiqui03@gmail.com>
2025-06-18 05:57:52 +0530

    Add Assign Cookies

--- Assign_Cookies.java
+++ Assign_Cookies.java
@@ -3,14 +3,10 @@ class Solution {
         Arrays.sort(g);
         Arrays.sort(s);
         int cnt=0;
-
-        for(int i=0 ; i< g.length ; i++){
-            for(int j=0 ; j< s.length ; j++){
-                if(s[j]>=g[i] && (s[j]!=0 && g[i]!=0)){
-                    cnt++;
-                    s[j]=0;
-                    g[i]=0;
-                }
+        int n = Math.min(g.length , s.length);
+        for(int i=0 ; i< n ; i++){
+            if(s[i]>=g[i]){
+                cnt++;
             }
         }
         return cnt;
