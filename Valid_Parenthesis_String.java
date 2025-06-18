fb0c97b0856ea0d25f3b974467e0fc99e72a9175
mudassir0002 <mudassirsiddiqui03@gmail.com>
2025-06-18 08:46:28 +0530

    Add Valid Parenthesis String

--- Valid_Parenthesis_String.java
+++ Valid_Parenthesis_String.java
@@ -1,53 +1,22 @@
 class Solution {
     public boolean checkValidString(String s) {
-        int n = s.length();
-        if(n==0){
-            return true;
-        }
-        char ch[] = s.toCharArray();
-        // if(n==1 && ch[0]=='*'){
-        //     return true;
-        // }else{
-        //     return false;
-        // }
-        int st=0,e=0;
-        for(int i=0 ; i<ch.length ; i++){
-            if(ch[i]=='('){
-                st++;
-            }else if(ch[i]==')'){
-                st--;
+        int low = 0, high = 0;
+
+        for (char ch : s.toCharArray()) {
+            if (ch == '(') {
+                low++;
+                high++;
+            } else if (ch == ')') {
+                if (low > 0) low--;
+                high--;
+            } else { // '*'
+                if (low > 0) low--;  // '*' as ')'
+                high++;             // '*' as '('
             }
-        }
-        if(st==0){
-            return true;
-        }else if(st<0){
-            e=0;
-            for(int i=0 ; i<ch.length ; i++){
-                if(ch[i]=='*'){
-                    e++;
-                }else if(ch[i]==')' && e>0){
-                    st++;
-                    e--;
-                }
-            }
-        }else{
-            e=0;
-            for(int i=ch.length-1 ; i>0 ; i--){
-                if(ch[i]=='*'){
-                    e++;
-                }else if(ch[i]=='(' && e>0){
-                    st--;
-                    e--;
-                }
-            }
+
+            if (high < 0) return false;  // More ')' than '('
         }
 
-        if(st==0){
-            return true;
-        }else{
-            return false;
-        }
-        
-
+        return low == 0;
     }
 }
