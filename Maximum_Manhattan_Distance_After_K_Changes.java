a12ce6167d80b8fc581951042291d87d7c12baf1
mudassir0002 <mudassirsiddiqui03@gmail.com>
2025-06-20 07:14:46 +0530

    Add Maximum Manhattan Distance After K Changes

--- Maximum_Manhattan_Distance_After_K_Changes.java
+++ Maximum_Manhattan_Distance_After_K_Changes.java
@@ -1,43 +1,22 @@
 class Solution {
     public int maxDistance(String s, int k) {
-        int[] d = new int[4]; // N, S, E, W
-        char[] ch = s.toCharArray();
-
-        for (char c : ch) {
-            if (c == 'N') d[0]++;
-            else if (c == 'S') d[1]++;
-            else if (c == 'E') d[2]++;
-            else d[3]++;
+        int ans = 0;
+        int north = 0, south = 0, east = 0, west = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == 'N') north++;
+            else if (c == 'S') south++;
+            else if (c == 'E') east++;
+            else if (c == 'W') west++;
+            
+            int x = Math.abs(north - south);
+            int y = Math.abs(east - west);
+            int MD = x + y;      //MD =Manhattan Distance
+            int dis = MD + Math.min(2 * k, i + 1 - MD); // Ensure changes are at most k
+            ans = Math.max(ans, dis);
         }
-
-        int cnt = 0;
-
-        // Handle North-South pair
-        if (d[0] > d[1]) {
-            cnt += d[0];
-            int use = Math.min(k, d[1]);
-            cnt += use;
-            k -= use;
-        } else {
-            cnt += d[1];
-            int use = Math.min(k, d[0]);
-            cnt += use;
-            k -= use;
-        }
-
-        // Handle East-West pair
-        if (d[2] > d[3]) {
-            cnt += d[2];
-            int use = Math.min(k, d[3]);
-            cnt += use;
-            k -= use;
-        } else {
-            cnt += d[3]; // ✅ FIXED: use += here instead of =
-            int use = Math.min(k, d[2]);
-            cnt += use;
-            k -= use;
-        }
-
-        return cnt;
+        
+        return ans;
     }
 }
