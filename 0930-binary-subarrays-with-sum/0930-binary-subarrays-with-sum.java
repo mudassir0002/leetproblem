7842e09630ea4fe5a925a10609b10b0e693f47a6
Mudassir Ahmed <134137854+mudassir0002@users.noreply.github.com>
2025-06-26 12:06:40 +0530

    Time: 1854 ms (5.11%), Space: 47.5 MB (84.34%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
@@ -5,6 +5,7 @@ class Solution {
         int max=0;
         int n = nums.length;
         for(int i=0 ;i<n ; i++){
+            sum=0;
             for(int j=i ; j<n ;j++){
                 sum = sum + nums[j];
                 if(sum==goal){
