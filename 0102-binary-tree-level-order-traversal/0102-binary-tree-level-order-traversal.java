a4a1f7311a3d56957288f229b8d6e72f8059fec9
Mudassir Ahmed <134137854+mudassir0002@users.noreply.github.com>
2025-06-28 18:48:17 +0530

    Time: 1 ms (90.27%), Space: 45.4 MB (12.33%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -15,26 +15,51 @@
  */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> result = new ArrayList<>();
-        Queue<TreeNode> que=new LinkedList<>();
-        if (root == null) {
-            return result; 
-        }
-        que.add(root);
-        while(!que.isEmpty()){
-            int s=que.size();
-            ArrayList<Integer> sub=new ArrayList<>();
-            for(int i=0;i<s;i++){
-                if(que.peek().left!=null){
-                    que.add(que.peek().left);
-                }
-                if(que.peek().right!=null){
-                    que.add(que.peek().right);
-                }
-                sub.add(que.poll().val);
-            }
-            result.add(sub);
-        }
-     return result;    
+
+    List<List<Integer>> result = new ArrayList<>();
+    Queue<TreeNode> q = new LinkedList<>();
+    if(root==null){
+        return result;
     }
+    q.add(root);
+
+    while(!q.isEmpty()){
+        List<Integer> li = new ArrayList<>();
+        int s = q.size();
+        for(int i=0 ; i<s ; i++){
+            TreeNode node = q.poll();
+            li.add(node.val);
+            if(node.left!=null){
+                q.add(node.left);
+            }
+            if(node.right!=null){
+                q.add(node.right);
+            }
+        }
+        result.add(li);
+    }
+    return result;
+
+    //     List<List<Integer>> result = new ArrayList<>();
+    //     Queue<TreeNode> que=new LinkedList<>();
+    //     if (root == null) {
+    //         return result; 
+    //     }
+    //     que.add(root);
+    //     while(!que.isEmpty()){
+    //         int s=que.size();
+    //         ArrayList<Integer> sub=new ArrayList<>();
+    //         for(int i=0;i<s;i++){
+    //             if(que.peek().left!=null){
+    //                 que.add(que.peek().left);
+    //             }
+    //             if(que.peek().right!=null){
+    //                 que.add(que.peek().right);
+    //             }
+    //             sub.add(que.poll().val);
+    //         }
+    //         result.add(sub);
+    //     }
+    //  return result;
+    }    
 }
