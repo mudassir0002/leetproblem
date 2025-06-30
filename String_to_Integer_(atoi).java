2a0030533a1f07e7199bf30163922706343321f7
mudassir0002 <mudassirsiddiqui03@gmail.com>
2025-06-30 07:28:53 +0530

    Add String to Integer (atoi)

--- String_to_Integer_(atoi).java
+++ String_to_Integer_(atoi).java
@@ -1,41 +1,33 @@
 class Solution {
     public int myAtoi(String s) {
-        int ans = 0;
-        int sign = 1;
-        boolean flag = false;
-        char[] ch = s.toCharArray();
-
-        for (int i = 0; i < ch.length; i++) {
-            // Skip spaces
-            if (!flag && ch[i] == ' ') {
+        int ans=0;
+        boolean flag=false;
+        boolean sign=false;
+        char []ch = s.toCharArray();
+        for(int i=0 ; i<ch.length ;i++){
+            if(ch[i]==' '){
                 continue;
             }
-
-            // Check for sign
-            if (!flag && (ch[i] == '-' || ch[i] == '+')) {
-                sign = (ch[i] == '-') ? -1 : 1;
-                flag = true;
-                continue;
+            if(ch[i]=='-' && flag==false){
+                sign=true;
             }
-
-            // Check for digit
-            if (ch[i] >= '0' && ch[i] <= '9') {
-                flag = true;
-                int digit = ch[i] - '0';
-
-                // Overflow check
-                if (ans > (Integer.MAX_VALUE - digit) / 10) {
-                    return (sign == 1) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
-                }
-
-                ans = ans * 10 + digit;
-            } else {
-                // Break if non-digit character after starting number
-                if (flag) break;
-                else return 0;
+            if(ch[i]=='-' && flag==true){
+                break;
+            }
+            
+            if((ch[i]>='0' && ch[i]<='9')){
+                ans = ans*10 + (ch[i] - '0');
+                flag=true;
+            }else if(flag==true && ans>0){
+                break;
+            }
+            if(ch[i]>='a' && flag==false){
+                break;
             }
         }
-
-        return ans * sign;
+        if(sign==true){
+            ans = -1 * ans;
+        }
+        return ans;
     }
 }
