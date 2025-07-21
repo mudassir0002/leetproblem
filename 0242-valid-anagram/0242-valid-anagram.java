f220526dbfb1b3a327f76db2422df7e145e19530
Mudassir Ahmed <134137854+mudassir0002@users.noreply.github.com>
2025-07-21 14:05:13 +0530

    Time: 19 ms (12.81%), Space: 44.9 MB (25.03%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -17,17 +17,37 @@ class Solution {
 
         // // Compare the sorted character arrays
         // return Arrays.equals(sArray, tArray);
-        char m1[] = new char[27];
+        // int m1[] = new int[26];
         
-        for(int i=0 ;i<s.length() ; i++){
-            m1[s.charAt(i)-'a']++;
-            m1[t.charAt(i)-'a']--;
-        }
+        // for(int i=0 ;i<s.length() ; ++i){
+        //     m1[s.charAt(i) -'a']++;
+        // }
+        // for(int i=0 ;i<t.length() ; ++i){
+        //     m1[t.charAt(i)-'a']--;
+        // }
+        // for(int i=0 ; i<m1.length ; i++){
+        //     if(m1[i]!=0){
+        //         return false;
+        //     }
+        // }
+
+        HashMap<Character,Integer> map = new HashMap<>();
         for(int i=0 ; i<s.length() ; i++){
-            if(m1[i]!=0){
+            map.put(s.charAt(i) , map.getOrDefault(s.charAt(i) , 0) +1);
+        }
+
+        for(int i=0 ; i<t.length() ; i++){
+            if(map.get(t.charAt(i)) != null){
+                if(map.get(t.charAt(i))==1){
+                    map.remove(t.charAt(i));
+                }else{
+                    map.put(t.charAt(i) , map.get(t.charAt(i))-1);
+                }
+            }else{
                 return false;
             }
         }
+
         return true;
     }
 }
