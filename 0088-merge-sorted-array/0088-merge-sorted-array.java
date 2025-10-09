465b4a7bc45b810db5a6c18e26aac8a2b699a866
Mudassir Ahmed <134137854+mudassir0002@users.noreply.github.com>
2025-10-09 12:01:02 +0530

    Time: 0 ms (100%), Space: 42.1 MB (90.79%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,33 +1,33 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
 
-        int[] arr = new int[m + n]; // Corrected size
-        int l1 = 0, l2 = 0;
-        int i = 0;
+        // int[] arr = new int[m + n]; // Corrected size
+        // int l1 = 0, l2 = 0;
+        // int i = 0;
 
-        // Compare elements from nums1 (up to m) and nums2 (up to n)
-        while (l1 < m && l2 < n) {
-            if (nums1[l1] < nums2[l2]) {
-                arr[i++] = nums1[l1++];
-            } else {
-                arr[i++] = nums2[l2++];
-            }
-        }
+        // // Compare elements from nums1 (up to m) and nums2 (up to n)
+        // while (l1 < m && l2 < n) {
+        //     if (nums1[l1] < nums2[l2]) {
+        //         arr[i++] = nums1[l1++];
+        //     } else {
+        //         arr[i++] = nums2[l2++];
+        //     }
+        // }
 
-        // Copy remaining elements from nums1 (if any)
-        while (l1 < m) {
-            arr[i++] = nums1[l1++];
-        }
+        // // Copy remaining elements from nums1 (if any)
+        // while (l1 < m) {
+        //     arr[i++] = nums1[l1++];
+        // }
 
-        // Copy remaining elements from nums2 (if any)
-        while (l2 < n) {
-            arr[i++] = nums2[l2++];
-        }
+        // // Copy remaining elements from nums2 (if any)
+        // while (l2 < n) {
+        //     arr[i++] = nums2[l2++];
+        // }
 
-        // Copy merged result back to nums1
-        for (i = 0; i < m + n; i++) {
-            nums1[i] = arr[i];
-        }
+        // // Copy merged result back to nums1
+        // for (i = 0; i < m + n; i++) {
+        //     nums1[i] = arr[i];
+        // }
 
 
         // int p1 = m - 1; // Pointer for the last element in nums1 (excluding extra space)
@@ -52,5 +52,51 @@ class Solution {
         //     p2--;
         //     p--;
         // }
+
+        // int arr[] = new int[m+n];
+        // int l=0;
+        // int r=0;
+        // int k=0;
+        // while(l<m && r<n){
+        //     if(nums1[l] < nums2[r] ){
+        //         arr[k++]=nums1[l++];
+        //     }else{
+        //         arr[k++]=nums2[r++];
+        //     }
+        // }
+
+        // while(l<m){
+        //     arr[k++]=nums1[l++];
+        // }
+
+        // while(r<n){
+        //     arr[k++]=nums2[r++];
+        // }
+
+        // for(int i=0 ;i<n+m ; i++){
+        //     nums1[i]=arr[i];
+        // }
+        int l=0;
+        int r=0;
+        int k=0;
+        int arr[] = new int[m+n];
+        while(l < m && r < n){
+            if(nums1[l]<nums2[r]){
+                arr[k++] = nums1[l++];
+            }else{
+                arr[k++] = nums2[r++];
+            }
+        }
+        while(l < m){
+            arr[k++] = nums1[l++];
+        }
+
+        while(r < n){
+            arr[k++] = nums2[r++];
+        }
+        for(int i=0 ;i<n+m ; i++){
+            nums1[i]=arr[i];
+        }
+
     }
 }
